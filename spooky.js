01e8b3c287ae3bced939f6014fd7d07bffb0d00f
Finn <finnterdal@gmail.com>
2019-05-08 09:51:12 -0500

    demo raw query

--- spooky.js
+++ spooky.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 
 const db = new Sequelize('postgres://localhost/mini-sequelize', {
   logging: false,
-  operatorsAliases: false
+  operatorsAliases: false,
 })
 
 const Witch = db.define('witch', {
@@ -13,31 +13,61 @@ const Witch = db.define('witch', {
   },
   age: {
     type: Sequelize.FLOAT,
-    validate: { min: 0, max: 90 }
-  }
+    validate: { min: 0, max: 9000 },
+  },
 })
+
+const Familiar = db.define('familiar', {
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  species: {
+    type: Sequelize.ENUM('cat', 'bat', 'toad'),
+    defaultValue: 'cat',
+  },
+})
+
+Familiar.belongsTo(Witch)
+Witch.hasMany(Familiar)
+
+const witches = [
+  {
+    name: 'Gertrude',
+    age: 45.75,
+  },
+  {
+    name: 'Baba Yaga',
+    age: 1000,
+  },
+]
+
+const familiars = [
+  {
+    name: 'Flappy',
+    species: 'bat',
+  },
+  {
+    name: 'Fluffy',
+  },
+]
 
 const halloween = async () => {
   try {
     await db.sync({ force: true })
-    const witches = [
-      {
-        name: 'Gertrude',
-        age: 45.75
-      },
-      {
-        name: 'Baba Yaga',
-        age: 1000
-      },
-    ]
-    // Apparently, bulkCreate doesn't run any of the validations. It totally
-    // ignores the min max stuff, and Postgres doesn't know about these
-    // validations
-    await Witch.bulkCreate(witches)
-    // await Witch.create({
-    //   name: 'Baba Yaga',
-    //   age: 1000
-    // })
+    const [[gertrude, baba], [flappy, fluffy]] = await Promise.all([
+      Witch.bulkCreate(witches),
+      Familiar.bulkCreate(familiars, { returning: true }),
+    ])
+    // console.log(fluffy.name)
+    // console.log(fluffy.__proto__)
+    // await Promise.all([flappy.setWitch(baba), fluffy.setWitch(baba)])
+    const aWitch = await Witch.findOne({
+      where: { id: 1 },
+      raw: true
+    })
+    console.log(aWitch)
     db.close()
   } catch (err) {
     console.error(err)
