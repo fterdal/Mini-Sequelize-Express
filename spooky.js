3c5739c5cf484dc034baf8b48431c2efa2bdc60a
Finn <finnterdal@gmail.com>
2019-06-13 11:02:18 -0500

    a join table can have associations and magic methods?@! 🧙‍♀️

--- spooky.js
+++ spooky.js
@@ -29,6 +29,10 @@ const Familiar = db.define('familiar', {
   },
 })
 
+const Friendship = db.define('friendship', {
+  longTerm: Sequelize.BOOLEAN,
+})
+
 const seedWitches = [
   {
     name: 'Sabrina',
@@ -50,12 +54,36 @@ const seedFamiliars = [
   },
 ]
 
+Witch.belongsToMany(Familiar, { through: Friendship })
+Familiar.belongsToMany(Witch, { through: Friendship })
+Friendship.belongsTo(Witch)
+Friendship.belongsTo(Familiar)
+
 const seed = async () => {
   try {
     await db.sync({ force: true })
-    console.log('hello')
-    const witches = await Witch.bulkCreate(seedWitches)
-    console.log(witches.map(witch => witch.name))
+
+    const witches = await Promise.all(seedWitches.map(wch => Witch.create(wch)))
+    const familiars = await Promise.all(
+      seedFamiliars.map(fam => Familiar.create(fam))
+    )
+
+    const [witch1, witch2] = witches
+    const [familiar1] = familiars
+
+    await Promise.all([
+      Friendship.create({ witchId: witch1.id, familiarId: familiar1.id }),
+      Friendship.create({ witchId: witch2.id, familiarId: familiar1.id }),
+    ])
+
+    // console.log(await witch1.getFamiliars())
+    // console.log(await familiar1.getWitches())
+
+    const friendships = await Friendship.findAll({
+      where: { witchId: witch1.id },
+      include: [{ model: Familiar }],
+    })
+    console.log(friendships.map(fr => fr.familiar))
   } catch (err) {
     console.error(err)
     db.close()
